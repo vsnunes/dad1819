6ead8c49a6fb36d81b271a094504d44c94a17f9f
Vitor Nunes <vitor.sobrinho.nunes@sapo.pt>
2018-10-26 13:44:41 +0100

    Adds TupleSpace Interface

---
+++ DIDA-TUPLE/Dida_Library/TupleSpace.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIDA_LIBRARY
+{
+    /// <summary>
+    /// An interface for describing basic Tuple Spaces operations.
+    /// </summary>
+    public interface TupleSpace
+    {
+        /// <summary>
+        /// Reads a tuple from the tuple space.
+        /// </summary>
+        /// <param name="tuple">A tuple to match the read.</param>
+        /// <returns>A taple matching the param.</returns>
+        Tuple read(Tuple tuple);
+
+        /// <summary>
+        /// Writes a new tuple to the tuple space.
+        /// </summary>
+        /// <param name="tuple">A tupple to be written.</param>
+        void write(Tuple tuple);
+
+        /// <summary>
+        /// Reads and takes a tuple from the tuple space.
+        /// </summary>
+        /// <param name="tuple">A tuple to take.</param>
+        /// <returns>A taple matching the param.</returns>
+        Tuple take(Tuple tuple);
+    }
+}
