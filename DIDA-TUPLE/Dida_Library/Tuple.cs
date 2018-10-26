d61f259d1d36bcc834ef3c69a1c6a841484e586a
Vitor Nunes <vitor.sobrinho.nunes@sapo.pt>
2018-10-26 15:34:55 +0100

    Adds WildComparator to Tuple missing tests

--- DIDA-TUPLE/Dida_Library/Tuple.cs
+++ DIDA-TUPLE/Dida_Library/Tuple.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace DIDA_LIBRARY
 {
@@ -55,10 +56,28 @@ namespace DIDA_LIBRARY
             return _types;
         }
 
+        private bool WildComparator(string s1, string s2)
+        {
+            if (s1.StartsWith("*") || s1.EndsWith("*"))
+            {
+                s1.Replace("*", ".*");
+                return Regex.IsMatch(s2, s1);
+            }
+
+            if (s2.StartsWith("*") || s2.EndsWith("*"))
+            {
+                s2.Replace("*", ".*");
+                return Regex.IsMatch(s1, s2);
+            }
+
+            return s1.Equals(s2);
+        }
+
         public override bool Equals(object obj)
         {
             if (obj.GetType() == this.GetType())
             {
+
                 Tuple tuple = obj as Tuple;
                 int numberOfFields = this.GetNumberOfFields();
                 if (tuple.GetNumberOfFields() == numberOfFields)
@@ -66,6 +85,13 @@ namespace DIDA_LIBRARY
                     List<Object> otherObjects = tuple.GetAllFields();
                     for (int i = 0; i < numberOfFields; i++)
                     {
+                        //When tuple fields are string instead of calling equals method call the wildcomparator
+                        //to check for wildcards in strings
+                        if (this.GetFieldByNumber(i).GetType() == typeof(string))
+                        {
+                            return WildComparator(this.GetFieldByNumber(i) as string, tuple.GetFieldByNumber(i) as string);
+                        }
+
                         if (!(this.GetFieldByNumber(i).Equals(tuple.GetFieldByNumber(i))))
                         {
                             return false;
