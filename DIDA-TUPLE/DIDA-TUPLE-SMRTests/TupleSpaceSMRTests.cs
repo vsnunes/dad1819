6a5f15a5c09fc79545a1f80386d9504e32771c30
mbelem <mbelem@sysmail.me>
2018-10-27 11:46:43 +0100

    adds tuplespace tests

---
+++ DIDA-TUPLE/DIDA-TUPLE-SMRTests/TupleSpaceSMRTests.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DIDA_TUPLE_SMR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIDA_TUPLE_SMR.Tests
+{
+    [TestClass()]
+    public class TupleSpaceSMRTests
+    {
+        //aqui mete as cenas comuns que precisas
+        [TestInitialize]
+        public void TestInitialize()
+        {
+           
+        }
+        [TestMethod()]
+        public void readTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void takeTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void writeTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
