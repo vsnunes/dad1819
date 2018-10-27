e900cccc0f0b0da696352c3e64b8fa13e476d2d8
Tiago Gonçalves <tiago.miguel.cg@gmail.com>
2018-10-27 12:45:29 +0100

    Adds XL core implementation

---
+++ DIDA-TUPLE/DIDA-TUPLE-XL/TupleSpaceXL.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DIDA_LIBRARY;
+using Tuple = DIDA_LIBRARY.Tuple;
+
+namespace DIDA_TUPLE_XL
+{
+    public class TupleSpaceXL : MarshalByRefObject, ITupleSpace
+    {
+        //Possibilidade de hashtable
+        private List<Tuple> _tupleSpace;
+
+        public TupleSpaceXL()
+        {
+            _tupleSpace = new List<Tuple>();
+        }
+
+        public int ItemCount()
+        {
+            return _tupleSpace.Count();
+        }
+
+        public Tuple read(Tuple tuple)
+        {
+            foreach (Tuple pos in _tupleSpace)
+            {
+                if (pos.Equals(tuple))
+                {
+                    return pos;
+                }
+            }
+            return null; //temos de incluir o hold para uma queue
+        }
+
+        /// <summary>
+        /// Takes a tuple from the tuple space.
+        /// </summary>
+        /// <param name="tuple">The tuple to be taken.</param>
+        /// <returns></returns>
+        public Tuple take(Tuple tuple)
+        {
+            Tuple match = null;
+
+            foreach (Tuple t in _tupleSpace)
+            {
+                if (t.Equals(tuple))
+                {
+                    match = t;
+                    break; //just found one so no need to continue searching
+                }
+            }
+
+            if (match != null)
+            {
+                lock (this) { _tupleSpace.Remove(match); }
+                return match;
+            }
+
+            //not found 
+            return null;
+        }
+
+        public void write(Tuple tuple)
+        {
+            _tupleSpace.Add(tuple);
+        }
+    }
+}
